26563e9dc88f76e684556f459e0f7e9a28e076fa
mfdsix <mfdsix.1nd0@gmail.com>
2024-02-12 11:01:36 +0700

    first initialize

---
+++ src/test/java/tech/zgenit/limitservice/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zgenit.limitservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
